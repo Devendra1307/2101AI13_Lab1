9f8e2520dd0f64dca7b605b24a7053d6fd4b1dae
Devendra <devendra2101ai13@gmail.com>
2022-07-27 23:00:21 +0530

    Selection sort

---
+++ Sorting.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+void Printarray(int *a,int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void selectionSort(int arr[], int size)
+{
+    int i, j;
+    for (i = 0 ;  i < size;i++)
+    {
+        for (j = i ; j < size; j++)
+        {
+            if (arr[i] > arr[j])
+                swap(&arr[i], &arr[j]);
+        }
+    }
+}
+
+int main()
+{
+    printf("Enter the size of array\n");
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter elements of array\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Choose your desired sorting algorithm:\n");
+    printf("1. Selection Sort\n");
+    printf("2. Insertion Sort\n");
+    printf("3. Bubble Sort\n");
+    printf("4. Merge Sort\n");
+    printf("5. Quick Sort\n");
+    int select;
+    scanf("%d",&select);
+    if(select==1){
+        selectionSort(a,n);
+        Printarray(a,n);
+    }
+    return  0;
+}
